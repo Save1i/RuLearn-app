4017652713703f35e35fe85460ad6af7cfa77dd4
Save1i <savva0512@gmail.com>
2025-03-05 13:41:41 +0300

    added user and section

--- server/modules/modules.js
+++ server/modules/modules.js
@@ -1,5 +1,17 @@
+const sequelize = require("../db")
+const {DataTypes} = require('sequelize')
 
+const User = sequelize.define('user', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    email: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'},
+})
 
+const Section = sequelize.define('section', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING},
+})
 
 
 
