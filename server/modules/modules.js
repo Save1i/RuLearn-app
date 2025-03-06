0d6cf128589d11bc231367b968c67c17c4d5f6f4
Save1i <savva0512@gmail.com>
2025-03-06 10:56:34 +0300

    fixed bugs and учзщкеув modules

--- server/modules/modules.js
+++ server/modules/modules.js
@@ -2,37 +2,36 @@ const sequelize = require("../db")
 const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
-    email: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: 'USER'},
 })
 
 const Statistics = sequelize.define('statistics', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    daily_streak: {type: DataTypes.NUMBER, defaultValue: 0},
-    daily_streak_max: {type: DataTypes.NUMBER, allowNull: false},
-    l_words: {type: DataTypes.NUMBER, allowNull: false},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    daily_streak: {type: DataTypes.INTEGER, defaultValue: 0},
+    daily_streak_max: {type: DataTypes.INTEGER, allowNull: false},
+    l_words: {type: DataTypes.INTEGER, allowNull: false},
 })
 
 const Section = sequelize.define('section', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
 })
 
 const Task = sequelize.define('task', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     type: {type: DataTypes.STRING, allowNull: false},
     duration: {type: DataTypes.STRING, allowNull: false},
     completed: {type: DataTypes.BOOLEAN, defaultValue: false},
-    score: {type: DataTypes.NUMBER, defaultValue: 0},
+    score: {type: DataTypes.INTEGER, defaultValue: 0},
 })
 
 const Test = sequelize.define('test', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
-    audio_q: {type: DataTypes.BYTEA, allowNull: true},
+    audio_q: {type: DataTypes.STRING, allowNull: true},
     text_q: {type: DataTypes.STRING, defaultValue: ""},
     options: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
     correct_answer: {type: DataTypes.STRING, allowNull: false},
@@ -41,11 +40,11 @@ const Test = sequelize.define('test', {
 
 
 const Library = sequelize.define('library', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
 
 const Dictionary = sequelize.define('dictionary', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     source_language: {type: DataTypes.STRING, allowNull: false, defaultValue: "CHS"},
     target_language: {type: DataTypes.STRING, allowNull: false, defaultValue: "RU"},
@@ -53,11 +52,11 @@ const Dictionary = sequelize.define('dictionary', {
 })
 
 const Word = sequelize.define('word', {
-    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     word_source: {type: DataTypes.STRING, allowNull: false, defaultValue: "CHS"},
     word_target: {type: DataTypes.STRING, allowNull: false, defaultValue: "RU"},
-    status: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false, defaultValue: "Новое слово"},
+    status: {type: DataTypes.STRING, allowNull: false, defaultValue: "Новое слово"},
     repeat_show: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
 })
 
@@ -83,6 +82,17 @@ Dictionary.belongsTo(Library)
 Dictionary.hasMany(Word)
 Word.belongsTo(Dictionary)
 
+
+module.exports = {
+    User,
+    Statistics,
+    Section,
+    Task,
+    Test,
+    Library,
+    Dictionary,
+    Word
+}
 
 
 
