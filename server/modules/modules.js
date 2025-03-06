f21c918706ecf641309a4443d070312d12c5bcb5
Save1i <savva0512@gmail.com>
2025-03-06 09:48:40 +0300

    added all modules

--- server/modules/modules.js
+++ server/modules/modules.js
@@ -8,10 +8,59 @@ const User = sequelize.define('user', {
     role: {type: DataTypes.STRING, defaultValue: 'USER'},
 })
 
+const Statistics = sequelize.define('statistics', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    daily_streak: {type: DataTypes.NUMBER, defaultValue: 0},
+    daily_streak_max: {type: DataTypes.NUMBER, allowNull: false},
+    l_words: {type: DataTypes.NUMBER, allowNull: false},
+})
+
 const Section = sequelize.define('section', {
     id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
 })
+
+const Task = sequelize.define('task', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    type: {type: DataTypes.STRING, allowNull: false},
+    duration: {type: DataTypes.STRING, allowNull: false},
+    completed: {type: DataTypes.BOOLEAN, defaultValue: false},
+    score: {type: DataTypes.NUMBER, defaultValue: 0},
+})
+
+const Test = sequelize.define('test', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    audio_q: {type: DataTypes.BYTEA, allowNull: true},
+    text_q: {type: DataTypes.STRING, defaultValue: ""},
+    options: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false},
+    correct_answer: {type: DataTypes.STRING, allowNull: false},
+    completed: {type: DataTypes.BOOLEAN, defaultValue: false},
+})
+
+
+const Library = sequelize.define('library', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Dictionary = sequelize.define('dictionary', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    source_language: {type: DataTypes.STRING, allowNull: false, defaultValue: "CHS"},
+    target_language: {type: DataTypes.STRING, allowNull: false, defaultValue: "RU"},
+    selected: {type: DataTypes.BOOLEAN, defaultValue: false},
+})
+
+const Word = sequelize.define('word', {
+    id: {types: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    word_source: {type: DataTypes.STRING, allowNull: false, defaultValue: "CHS"},
+    word_target: {type: DataTypes.STRING, allowNull: false, defaultValue: "RU"},
+    status: {type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false, defaultValue: "Новое слово"},
+    repeat_show: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
+})
+
 
 
 
