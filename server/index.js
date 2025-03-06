46d31f7394d99853cee36eec04081825c96955a1
Save1i <savva0512@gmail.com>
2025-03-06 11:16:14 +0300

    added cors and connected with app

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const modules = require("./modules/modules")
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
