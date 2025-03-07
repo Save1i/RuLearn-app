7b8bddb57f4b27995721856c91468562ae43f457
Save1i <savva0512@gmail.com>
2025-03-07 13:13:43 +0300

    added getAll sections controller logic

--- server/controllers/sectionController.js
+++ server/controllers/sectionController.js
@@ -4,12 +4,14 @@ const ApiError = require('../error/apiError')
 class SectionController {
     async create(req, res) {
         const {name} = req.body
+
         const section = await Section.create({name})
         return res.json(section) 
     }
 
     async getAll(req, res) {
-        
+        const sections = await Section.findAll()
+        return res.json(sections)
     }
 
     async getOne(req, res) {
