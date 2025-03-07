8df29ea92a39fcd43f41ec03f656fe8958431811
Save1i <savva0512@gmail.com>
2025-03-07 13:44:06 +0300

    added create task controller logic

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,6 +1,12 @@
+const {Task} =require('../modules/modules')
+const ApiError = require('../error/apiError')
+
 class TaskController {
     async create(req, res) {
+        const {name, type, duration, completed} = req.body
         
+        const task = await Task.create({name, type, duration, completed})
+        return res.json(task) 
     }
 
     async getAll(req, res) {
