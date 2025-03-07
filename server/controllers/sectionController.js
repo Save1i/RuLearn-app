a396b249c72ec5e32bc4d985b72d597fea301052
Save1i <savva0512@gmail.com>
2025-03-07 12:54:46 +0300

    added create controller

--- server/controllers/sectionController.js
+++ server/controllers/sectionController.js
@@ -1,9 +1,18 @@
+const {Section} =require('../modules/modules')
+const ApiError = require('../error/apiError')
+
 class SectionController {
     async create(req, res) {
-        
+        const {name} = req.body
+        const section = await Section.create({name})
+        return res.json(section) 
     }
 
     async getAll(req, res) {
+        
+    }
+
+    async getOne(req, res) {
         
     }
 
