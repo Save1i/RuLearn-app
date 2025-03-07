1fbd7421b3139100f773b62ec250fbda00a52e0a
Save1i <savva0512@gmail.com>
2025-03-07 14:54:24 +0300

    added create test controller logic

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,8 +1,28 @@
+const uuid = require('uuid')
+const path = require('path')
+
 const {Test} =require('../modules/modules')
 const ApiError = require('../error/apiError')
 
 class TestController {
-    async create(req, res) {
+    async create(req, res, next) {
+        try {
+        const {name, text_q, options, correct_answer, completed} = req.body
+        
+        const {img} = req.files
+        let fileName = uuid.v4() + ".png"
+        img.mv(path.resolve(__dirname, "..", "static", fileName))
+        
+        const {audio_q} = req.files
+        let audioFileName = uuid.v4() + ".mp3"
+        audio_q.mv(path.resolve(__dirname, "..", "static", audioFileName))
+
+        const test = await Test.create({name, text_q, options, correct_answer, img: fileName, audio_q: audioFileName, completed})
+        return res.json(test) 
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+        
    
     }
 
