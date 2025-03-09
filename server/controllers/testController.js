081e927b3ab08c49ec25048f89f1baeaa3b2a8fb
Save1i <savva0512@gmail.com>
2025-03-09 15:44:46 +0300

    deleted "complete" in req.body and added taskId

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -7,7 +7,7 @@ const ApiError = require('../error/apiError')
 class TestController {
     async create(req, res, next) {
         try {
-        const {name, text_q, options, correct_answer, completed} = req.body
+        const {name, text_q, options, correct_answer, taskId} = req.body
         
         const {img} = req.files
         let fileName = uuid.v4() + ".png"
@@ -17,7 +17,7 @@ class TestController {
         let audioFileName = uuid.v4() + ".mp3"
         audio_q.mv(path.resolve(__dirname, "..", "static", audioFileName))
 
-        const test = await Test.create({name, text_q, options, correct_answer, img: fileName, audio_q: audioFileName, completed})
+        const test = await Test.create({name, text_q, options, correct_answer, img: fileName, audio_q: audioFileName, taskId})
         return res.json(test) 
         } catch (e) {
             next(ApiError.badRequest(e.message))
