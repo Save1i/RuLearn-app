5bdabf57a97a83413b231ca66fffebea5822e7a1
Save1i <savva0512@gmail.com>
2025-03-10 12:00:11 +0300

    added checkRole

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const taskController = require('../controllers/taskController')
+const checkRole = require("../middleware/chekRoleMiddleware")
 
-router.post('/', taskController.create)
+router.post('/',checkRole('ADMIN'), taskController.create)
 router.get('/', taskController.getAll)
 router.get('/:id', taskController.getOne)
 
