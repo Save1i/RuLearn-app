e0a78fde71efc90e1e9c9e67e9dafc5e78581c83
Save1i <savva0512@gmail.com>
2025-03-10 11:59:51 +0300

    added checkRole for create section

--- server/routes/sectionRouter.js
+++ server/routes/sectionRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const sectionController = require('../controllers/sectionController')
+const checkRole = require("../middleware/chekRoleMiddleware")
 
-router.post('/', sectionController.create)
+router.post('/',checkRole('ADMIN'), sectionController.create)
 router.get('/', sectionController.getAll)
 router.get('/:id', sectionController.getOne)
 
