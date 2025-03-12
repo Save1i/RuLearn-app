d68f3fee6fec8acc2c3201090f31427e3f8612e3
Save1i <savva0512@gmail.com>
2025-03-12 11:43:58 +0300

    added Context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import {AppRouter} from './components/AppRouter.tsx';
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import UserStore from './store/UserStore';
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AppRouter />
-  </BrowserRouter>
-)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+interface ContextProps {
+  user: UserStore;
+}
+
+export const Context = createContext<ContextProps | null>(null);
+
+
+root.render(
+  <Context.Provider value={{
+    user: new UserStore()}}>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+    </Context.Provider>
+
+);
