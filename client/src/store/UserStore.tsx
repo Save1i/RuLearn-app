071064215c9fefba38a46a44d7e51779b5fa8d66
Save1i <savva0512@gmail.com>
2025-03-12 11:01:19 +0300

    added class UserStore

---
+++ client/src/store/UserStore.tsx
@@ -0,0 +1,35 @@
+import { makeAutoObservable } from "mobx"
+
+interface User {
+    id?: number,
+    email?: string,
+    role?: string,
+    password?: string
+}
+
+export default class UserStore {
+    private _isAuth: boolean;
+    private _user: User;
+
+  constructor() {
+    this._isAuth = true
+    this._user = {}
+    makeAutoObservable(this)
+  }
+
+  setIsAuth(bool: boolean) {
+    this._isAuth = bool
+  }
+
+  setIsUser(user: object) {
+    this._user = user
+  }
+
+  get isAuth() {
+    return this._isAuth
+  }
+
+  get user() {
+    return this._user
+  }
+}
