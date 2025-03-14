8e243c269a4e8f6b5f98f6fb6c7b4a5710c783d7
Save1i <savva0512@gmail.com>
2025-03-14 14:42:49 +0300

    change path for test_toute + taskId

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,32 +1,37 @@
-import Admin  from "./pages/Admin";
+import Admin from "./pages/Admin";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 import Test from "./pages/Test";
-import { ADMIN_ROUTE, HOME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, TEST_ROUTE } from "./utils/consts";
+import {
+  ADMIN_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  REGISTRATION_ROUTE,
+  TEST_ROUTE,
+} from "./utils/consts";
 
 export const authRoutes = [
-    {
-        path: ADMIN_ROUTE,
-        Component: Admin    
-    },
-    {
-        path: TEST_ROUTE + "/:id",
-        Component: Test 
-    }
-]
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin,
+  },
+  {
+    path: TEST_ROUTE + "/:taskId",
+    Component: Test,
+  },
+];
 
 export const publicRoutes = [
-    {
-        path: LOGIN_ROUTE,
-        Component: Auth    
-    },
-    {
-        path: REGISTRATION_ROUTE,
-        Component: Auth 
-    },
-    {
-        path: HOME_ROUTE,
-        Component: Home    
-    }
-
-]
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: HOME_ROUTE,
+    Component: Home,
+  },
+];
