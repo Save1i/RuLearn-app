451161140d7bee2133a58addfe9f32133487c1e2
Save1i <savva0512@gmail.com>
2025-03-14 14:43:42 +0300

    create home page and add sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,31 @@
+import { useContext } from "react";
+import Section from "./Section";
+import { Context } from "../main";
+import styles from "../styles/home.module.css";
 
-export default function Home() {
+const Home = () => {
+  const context = useContext(Context);
+
+  if (!context) {
+    console.error("context не найден!");
+    return null;
+  }
+
+  const { home } = context;
+
   return (
-    <div>
-      <p>Home</p>
+    <div className={styles.home}>
+      <div className={styles.home__inner}>
+        <div className={styles.sections__container}>
+          {home.isTasks.length > 0 ? (
+            home.isSections.map((section) => <Section key={section.id} section={section} />)
+          ) : (
+            <p>Задач пока нет</p>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
