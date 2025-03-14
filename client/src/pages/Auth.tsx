bbffe33b30d1bb7c15186a963fdd7e4c59b48102
Save1i <savva0512@gmail.com>
2025-03-14 14:43:17 +0300

    create auth page

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,8 +1,29 @@
+import { Link, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 export default function Auth() {
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
   return (
-    <div>
-      Auth
+ <div>
+      <h2>{isLogin? "Авторизация" : "Регистрация"}</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Почта</label>
+          <input id="email" type="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Пароль</label>
+          <input id="password" type="password" />
+        </div>
+
+        <button type="submit">{isLogin ? "Войти" : "Регистрация"}</button>
+      </form>
+      {isLogin ? 
+        <p className="">Нет аккаунта? <Link to={REGISTRATION_ROUTE}>Регистрация</Link></p>
+        :
+        <p className="">Есть аккаунт? <Link to={LOGIN_ROUTE}>Войти</Link></p>
+      }
     </div>
   )
 }
