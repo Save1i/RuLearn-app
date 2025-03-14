577d20518ed60bd269c661b0dd35cf3c7bed152e
Save1i <savva0512@gmail.com>
2025-03-14 14:41:59 +0300

    added context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,26 @@
-import React, { createContext } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import UserStore from './store/UserStore';
-import HomeStore from './store/HomeStore';
+import "./index.css";
+import { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import UserStore from "./store/UserStore";
+import HomeStore from "./store/HomeStore";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 interface ContextProps {
-  user: UserStore,
-  home: HomeStore 
+  user: UserStore;
+  home: HomeStore;
 }
 
 export const Context = createContext<ContextProps | null>(null);
 
-
 root.render(
-  <Context.Provider value={{
-    user: new UserStore(),
-    home: new HomeStore(),
-    }}>
-  <React.StrictMode>
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+      home: new HomeStore(),
+    }}
+  >
     <App />
-  </React.StrictMode>
-    </Context.Provider>
-
+  </Context.Provider>
 );
