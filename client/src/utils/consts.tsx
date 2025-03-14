f24bccbf3a543f9edddf4074dc12183ad3eb007d
Save1i <savva0512@gmail.com>
2025-03-14 14:45:24 +0300

    some changes

--- client/src/utils/consts.tsx
+++ client/src/utils/consts.tsx
@@ -1,5 +1,7 @@
-export const ADMIN_ROUTE = "/admin"
-export const LOGIN_ROUTE = "/login"
+export const ADMIN_ROUTE = "/admin";
+export const LOGIN_ROUTE = "/login";
 export const REGISTRATION_ROUTE = "/registration";
-export const HOME_ROUTE = "/"
-export const TEST_ROUTE = "/test"
+export const HOME_ROUTE = "/";
+export const TEST_ROUTE = "/test";
+
+export const SERVER_URL = "http://localhost:5000/";
