398e78789b30baf808823aa834cb4780f80624df
Save1i <savva0512@gmail.com>
2025-03-16 11:44:29 +0300

    added bootstrap css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
-import "./index.css";
 import { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import UserStore from "./store/UserStore";
 import HomeStore from "./store/HomeStore";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
