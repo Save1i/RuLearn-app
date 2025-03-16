8bfc5d57fb09bcc4fbb7572e234f9abcac62c566
Save1i <savva0512@gmail.com>
2025-03-16 11:46:06 +0300

    save token in local storage

--- client/src/http/userAPI.ts
+++ client/src/http/userAPI.ts
@@ -1,17 +1,24 @@
-import { $host } from "./index";
+import { $authHost, $host } from "./index";
 import { jwtDecode } from "jwt-decode";
 
 export const registration = async (email: string, password: string) => {
   const { data } = await $host.post("api/user/registration", { email, password, role: "USER" });
+  localStorage.setItem("token", data.token);
   return jwtDecode(data.token);
 };
 
 export const login = async (email: string, password: string) => {
   const { data } = await $host.post("api/user/login", { email, password });
+
+  if (!data.token) {
+    throw new Error("Сервер не вернул токен");
+  }
+  localStorage.setItem("token", data.token);
   return jwtDecode(data.token);
 };
 
 export const check = async () => {
-  const response = await $host.post("api/user/auth");
-  return response;
+  const { data } = await $authHost.get("api/user/auth");
+  localStorage.setItem("token", data.token);
+  return jwtDecode(data.token);
 };
