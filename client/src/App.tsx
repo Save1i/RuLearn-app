50c76e0d63cbc98412a3eed73b265fd6d58d3a01
Save1i <savva0512@gmail.com>
2025-03-16 11:43:32 +0300

    create

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,48 @@
-import { BrowserRouter } from 'react-router-dom';
-import { AppRouter } from './components/AppRouter';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import { AppRouter } from "./components/AppRouter";
+import { observer } from "mobx-react-lite";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./main";
+import { check } from "./http/userAPI";
+import { Spinner } from "react-bootstrap";
 
-const App = () => {
+const App = observer(() => {
+  const context = useContext(Context);
+
+  if (!context) {
+    console.error("context не найден!");
+    return null;
+  }
+
+  const { user } = context;
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    check()
+      .then(() => {
+        user.setIsUser(true);
+        user.setIsAuth(true);
+      })
+      .catch((error) => {
+        console.error("Ошибка при проверке авторизации:", error.response?.data || error.message);
+        user.setIsUser(false);
+        user.setIsAuth(false);
+        localStorage.removeItem("token");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Spinner animation={"grow"} />;
+  }
+
   return (
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
