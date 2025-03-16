35ee99ac696eefc3c07c3ec7aac89775b23d9e52
Save1i <savva0512@gmail.com>
2025-03-16 09:56:39 +0300

    create reg log check requests

---
+++ client/src/http/userAPI.ts
@@ -0,0 +1,17 @@
+import { $host } from "./index";
+import { jwtDecode } from "jwt-decode";
+
+export const registration = async (email: string, password: string) => {
+  const { data } = await $host.post("api/user/registration", { email, password, role: "USER" });
+  return jwtDecode(data.token);
+};
+
+export const login = async (email: string, password: string) => {
+  const { data } = await $host.post("api/user/login", { email, password });
+  return jwtDecode(data.token);
+};
+
+export const check = async () => {
+  const response = await $host.post("api/user/auth");
+  return response;
+};
