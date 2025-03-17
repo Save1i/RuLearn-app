f33e2961d52e2821b63d07f8aa94b96b3d7bb75c
Save1i <savva0512@gmail.com>
2025-03-17 18:42:24 +0300

    rewrite add sections

--- client/src/pages/Section.tsx
+++ client/src/pages/Section.tsx
@@ -1,33 +1,32 @@
-import { useContext } from "react";
-import { Context } from "../main";
+import { observer } from "mobx-react-lite";
 import Task from "./Task";
+
+interface isTask {
+  id: number;
+  name: string;
+  type: string;
+  duration: string;
+  sectionId: number;
+}
 
 interface SectionProps {
   section: {
     id: number;
     name: string;
   };
+  tasks: Array<isTask>;
 }
 
-const Section: React.FC<SectionProps> = ({ section }) => {
-  const context = useContext(Context);
-
-  if (!context) {
-    console.error("context не найден!");
-    return null;
-  }
-
-  const { home } = context;
+const Section: React.FC<SectionProps> = observer(({ section, tasks }) => {
+  const sectionTasks = tasks.filter((task) => task.sectionId === section.id);
 
   return (
     <div className="section">
       <div className="section__inner">
         <h2 className="section__title">{section.name}</h2>
         <div className="tasks__container">
-          {home.isTasks.length > 0 ? (
-            home.isTasks.map((task) =>
-              task.sectionId === section.id ? <Task key={task.id} task={task} /> : false
-            )
+          {sectionTasks.length > 0 ? (
+            sectionTasks.map((task) => <Task key={task.id} task={task} />)
           ) : (
             <p>Задач пока нет</p>
           )}
@@ -35,6 +34,6 @@ const Section: React.FC<SectionProps> = ({ section }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Section;
