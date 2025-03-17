62ae738301de9d610ee833ffaf0d319dd23cc837
Save1i <savva0512@gmail.com>
2025-03-17 18:44:56 +0300

    added authMiddleware

--- server/routes/sectionRouter.js
+++ server/routes/sectionRouter.js
@@ -2,9 +2,10 @@ const Router = require('express')
 const router = new Router()
 const sectionController = require('../controllers/sectionController')
 const checkRole = require("../middleware/chekRoleMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post('/',checkRole('ADMIN'), sectionController.create)
-router.get('/', sectionController.getAll)
-router.get('/:id', sectionController.getOne)
+router.get('/', authMiddleware, sectionController.getAll)
+router.get('/:id', authMiddleware, sectionController.getOne)
 
 module.exports = router
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -2,9 +2,10 @@ const Router = require('express')
 const router = new Router()
 const taskController = require('../controllers/taskController')
 const checkRole = require("../middleware/chekRoleMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post('/',checkRole('ADMIN'), taskController.create)
-router.get('/', taskController.getAll)
-router.get('/:id', taskController.getOne)
+router.get('/', authMiddleware, taskController.getAll)
+router.get('/:id', authMiddleware, taskController.getOne)
 
 module.exports = router
