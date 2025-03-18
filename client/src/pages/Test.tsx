b9206fa792002749c1eea5790e4481d4b5b3f533
Save1i <savva0512@gmail.com>
2025-03-18 17:12:55 +0300

    using useState instead of global storage

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,42 +1,48 @@
-import { useContext, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Context } from "../main";
 import { fetchTests } from "../http/homeAPI";
 import { observer } from "mobx-react-lite";
 import AudioQuestion from "./AudiQuestion";
 
 console.log(import.meta.env.VITE_API_URL);
 
+interface Test {
+  id: number;
+  taskId: number;
+  name: string;
+  audio_q: string;
+  text_q: string;
+  options: string[];
+  correct_answer: string;
+  img: string;
+}
+
 const Test = observer(() => {
   const { taskId } = useParams();
-  const { home } = useContext(Context);
+
+  const [test, setTest] = useState<Test[]>([]);
 
   useEffect(() => {
-    fetchTests(taskId).then((data) => home.setTest(data.rows));
+    fetchTests(taskId).then((data) => setTest(data.rows));
   }, []);
 
   return (
     <div>
-      {home.isTests.map((test) => (
-        <div key={test.id} className="test">
+      {test.map((el) => (
+        <div key={el.id} className="test">
           <div className="test__inner">
             <div className="question">
-              <p className="question__title">{test.text_q}</p>
+              <p className="question__title">{el.text_q}</p>
               <div className="question__container">
-                <img
-                  width={100}
-                  height={125}
-                  src={import.meta.env.VITE_API_URL + test.img}
-                  alt=""
-                />{" "}
-                {test.audio_q ? (
-                  <AudioQuestion audio_q={test.audio_q} />
+                <img width={100} height={125} src={import.meta.env.VITE_API_URL + el.img} alt="" />{" "}
+                {el.audio_q ? (
+                  <AudioQuestion audio_q={el.audio_q} />
                 ) : (
-                  <p className="question__tex">{test.text_q}</p>
+                  <p className="question__tex">{el.text_q}</p>
                 )}
               </div>
               <div className="options">
-                {test.options.map((opt, index) => (
+                {el.options.map((opt, index) => (
                   <button key={index} className="options__btn">
                     {opt}
                   </button>
