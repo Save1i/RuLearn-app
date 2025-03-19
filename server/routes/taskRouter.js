2790a2a6a6f3c696bb73d6643b245b046877c35c
Save1i <savva0512@gmail.com>
2025-03-19 17:00:25 +0300

    added authMiddleware

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,11 +1,11 @@
-const Router = require('express')
-const router = new Router()
-const taskController = require('../controllers/taskController')
-const checkRole = require("../middleware/chekRoleMiddleware")
+const Router = require("express");
+const router = new Router();
+const taskController = require("../controllers/taskController");
+const checkRole = require("../middleware/chekRoleMiddleware");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post('/',checkRole('ADMIN'), taskController.create)
-router.get('/', authMiddleware, taskController.getAll)
-router.get('/:id', authMiddleware, taskController.getOne)
+router.post("/", checkRole("ADMIN"), taskController.create);
+router.get("/", authMiddleware, taskController.getAll);
+router.get("/:id", authMiddleware, taskController.getOne);
 
-module.exports = router
+module.exports = router;
