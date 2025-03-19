32733d89f29b51df107db34ee40f6eb5de6838aa
Save1i <savva0512@gmail.com>
2025-03-19 12:48:57 +0300

    removed mock data

--- client/src/store/HomeStore.tsx
+++ client/src/store/HomeStore.tsx
@@ -28,42 +28,17 @@ export default class HomeStore {
   private _tasks: Array<Task>;
   private _tests: Array<Test>;
   private _sections: Array<Section>;
+  private _page: number;
+  private _totalCount: number;
+  private _limit: number;
 
   constructor() {
     this._sections = [];
     this._tasks = [];
-    this._tests = [
-      {
-        id: 1,
-        taskId: 1,
-        name: "Привет",
-        audio_q: "https://voicebot.su/uploads/sounds/48/47235/47235.mp3",
-        text_q: "Что вы слышите?",
-        options: ["Привет", "Пока", "Хай", "Приветик"],
-        correct_answer: "Привет",
-        img: "https://img.freepik.com/free-vector/cute-lazy-cat-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector_138676-6706.jpg?t=st=1741853215~exp=1741856815~hmac=3ab4cd2579a3d4c681b160048a76d624e42f34afbc2a230c5d1e9cd6ac9b069c&w=900",
-      },
-      {
-        id: 2,
-        taskId: 1,
-        name: "Оу привет",
-        audio_q: "https://voicebot.su/uploads/sounds/48/47232/47232.mp3",
-        text_q: "Что вы слышите?",
-        options: ["Привет", "Пока", "Оу привет", "Приветик"],
-        correct_answer: "Оу привет",
-        img: "https://img.freepik.com/free-vector/cute-lazy-cat-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector_138676-6706.jpg?t=st=1741853215~exp=1741856815~hmac=3ab4cd2579a3d4c681b160048a76d624e42f34afbc2a230c5d1e9cd6ac9b069c&w=900",
-      },
-      {
-        id: 2,
-        taskId: 2,
-        name: "Оу привет",
-        audio_q: "https://voicebot.su/uploads/sounds/48/47232/47232.mp3",
-        text_q: "Что вы слышите?",
-        options: ["Привет", "Пока", "Оу привет", "Приветик"],
-        correct_answer: "Оу привет",
-        img: "https://img.freepik.com/free-vector/cute-lazy-cat-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector_138676-6706.jpg?t=st=1741853215~exp=1741856815~hmac=3ab4cd2579a3d4c681b160048a76d624e42f34afbc2a230c5d1e9cd6ac9b069c&w=900",
-      },
-    ];
+    this._tests = [];
+    this._page = 1;
+    this._totalCount = 0;
+    this._limit = 1;
 
     makeAutoObservable(this);
   }
@@ -72,12 +47,24 @@ export default class HomeStore {
     this._tasks = task;
   }
 
-  setTest(test: Test) {
-    this._tests.push(test);
+  setTest(test: Array<Test>) {
+    this._tests = test;
   }
 
   setSection(section: Array<Section>) {
     this._sections = section;
+  }
+
+  setPage(page: number) {
+    this._page = page;
+  }
+
+  setTotalCount(count: number) {
+    this._totalCount = count;
+  }
+
+  setLimit(limit: number) {
+    this._limit = limit;
   }
 
   get isSections() {
@@ -90,5 +77,17 @@ export default class HomeStore {
 
   get isTests() {
     return this._tests;
+  }
+
+  get isPage() {
+    return this._page;
+  }
+
+  get isTotalCount() {
+    return this._totalCount;
+  }
+
+  get isLimit() {
+    return this._limit;
   }
 }
