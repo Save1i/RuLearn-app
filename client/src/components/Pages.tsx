0b886950bbe6311459106cfac5ed53af8ecbc82c
Save1i <savva0512@gmail.com>
2025-03-19 13:59:45 +0300

    added navigate, answerProps and postTaskProgress after last test

--- client/src/components/Pages.tsx
+++ client/src/components/Pages.tsx
@@ -1,15 +1,27 @@
 import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 import { Context } from "../main";
+import { postTaskProgress } from "../http/postTaskProgress";
+import { useNavigate } from "react-router-dom";
+import { HOME_ROUTE } from "../utils/consts";
 
-const Pages = observer(() => {
+interface AnswersProps {
+  answer: string;
+  correctAnswer: string;
+  userId: number;
+  taskId: string | undefined;
+}
+
+const Pages: React.FC<AnswersProps> = observer(({ answer, correctAnswer, userId, taskId }) => {
   const { home } = useContext(Context);
   const pageCount = Math.ceil(home.isTotalCount / home.isLimit);
+
+  const navigate = useNavigate();
 
   // Проверяем, есть ли следующая страница
   const hasNextPage = home.isPage < pageCount;
 
-  console.log(pageCount);
+  console.log(pageCount, hasNextPage);
 
   const nextPage = () => {
     if (hasNextPage) {
@@ -19,7 +31,26 @@ const Pages = observer(() => {
 
   console.log(home.isPage);
 
-  return hasNextPage ? <button onClick={nextPage}>{home.isPage + 1}</button> : null;
+  return hasNextPage ? (
+    <div>
+      <p>{answer}</p>
+      <p>{correctAnswer}</p>
+      <button onClick={nextPage}>{home.isPage + 1}</button>
+    </div>
+  ) : (
+    <div>
+      <p>{answer}</p>
+      <p>{correctAnswer}</p>
+      <button
+        onClick={() => {
+          postTaskProgress(userId, Number(taskId));
+          navigate(HOME_ROUTE);
+        }}
+      >
+        {home.isPage + 1}
+      </button>
+    </div>
+  );
 });
 
 export default Pages;
