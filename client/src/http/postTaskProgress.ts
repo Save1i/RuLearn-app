6fbc02323d7b6f6b8b1f1a8277ee642d4dcdecde
Save1i <savva0512@gmail.com>
2025-03-20 16:00:59 +0300

    added update homeStore

--- client/src/http/postTaskProgress.ts
+++ client/src/http/postTaskProgress.ts
@@ -1,18 +1,25 @@
 import axios from "axios";
+import { fetchTaskProgress } from "./homeAPI";
+import HomeStore from "../store/HomeStore"; // путь к твоему стору
 
-export const postTaskProgress = async (userId: number, taskId: number) => {
+export const postTaskProgress = async (userId: number, taskId: number, home: HomeStore) => {
   try {
     const response = await axios.post(`${import.meta.env.VITE_API_URL}api/task-progress`, {
       userId,
       taskId,
     });
+
+    // Обновление прогресса задачи
+    const data = await fetchTaskProgress(userId);
+    home.setTaskProgress(data);
+
     console.log(response.data.message);
     return "correct!";
   } catch (error: unknown) {
-    if (typeof error === "object" && error !== null && "response" in error) {
-      console.error("Ошибка запроса:", error.response || error);
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка запроса:", error.response?.data || error.message);
     } else {
-      console.log("Неизвестная ошибка");
+      console.error("Неизвестная ошибка", error);
     }
   }
 };
