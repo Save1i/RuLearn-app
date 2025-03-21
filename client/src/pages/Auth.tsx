1c14d56d2e91ad861480daf3605d1510ba1d58b2
Save1i <savva0512@gmail.com>
2025-03-21 18:13:50 +0300

    create validateEmail

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,12 +1,12 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { HOME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 import { login, registration } from "../http/userAPI";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { Context } from "../main";
 import { fetchSections } from "../http/homeAPI";
 import styles from "../styles/auth.module.css";
-import authImage from "../img/auth.png"; // Импорт изображения
+import authImage from "../img/auth.png";
 
 const Auth = observer(() => {
   const navigate = useNavigate();
@@ -15,6 +15,22 @@ const Auth = observer(() => {
   const isLogin = location.pathname === LOGIN_ROUTE;
   const email = useRef<HTMLInputElement>(null);
   const password = useRef<HTMLInputElement>(null);
+
+  const [error, setError] = useState("");
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleBlur = () => {
+    if (!email.current) return;
+    if (!validateEmail(email.current.value)) {
+      setError("Введите корректный email");
+    } else {
+      setError("");
+    }
+  };
 
   if (!context) {
     console.error("context не найден!");
@@ -26,7 +42,7 @@ const Auth = observer(() => {
   const click = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
-      if (!email.current || !password.current) return;
+      if (!email.current || !password.current || error) return console.log("Ошибка в данных");
       let data;
       if (isLogin) {
         data = await login(email.current.value, password.current.value);
@@ -63,28 +79,46 @@ const Auth = observer(() => {
             <label htmlFor="email" className={styles.auth__label}>
               Почта
             </label>
-            <input id="email" type="email" ref={email} className={styles.auth__input} />
+            <input
+              onBlur={handleBlur}
+              id="email"
+              type="email"
+              ref={email}
+              className={`${error ? styles.auth__input_error : styles.auth__input}`}
+              required
+            />
           </div>
           <div className={styles.auth__inputGroup}>
             <label htmlFor="password" className={styles.auth__label}>
               Пароль
             </label>
-            <input id="password" type="password" ref={password} className={styles.auth__input} />
+            <input
+              id="password"
+              type="password"
+              ref={password}
+              className={styles.auth__input}
+              required
+            />
           </div>
-
+          {isLogin ? (
+            <p className={styles.auth__link}>
+              Нет аккаунта?{" "}
+              <Link className={styles.auth__swap} to={REGISTRATION_ROUTE}>
+                Регистрация
+              </Link>
+            </p>
+          ) : (
+            <p className={styles.auth__link}>
+              Есть аккаунт?{" "}
+              <Link className={styles.auth__swap} to={LOGIN_ROUTE}>
+                Войти
+              </Link>
+            </p>
+          )}
           <button onClick={(e) => click(e)} className={styles.auth__button}>
             {isLogin ? "Войти" : "Регистрация"}
           </button>
         </form>
-        {isLogin ? (
-          <p className={styles.auth__link}>
-            Нет аккаунта? <Link to={REGISTRATION_ROUTE}>Регистрация</Link>
-          </p>
-        ) : (
-          <p className={styles.auth__link}>
-            Есть аккаунт? <Link to={LOGIN_ROUTE}>Войти</Link>
-          </p>
-        )}
       </div>
     </div>
   );
