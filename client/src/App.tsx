67375979e60b324ab15686d732e61ce2c73d7b2a
Save1i <savva0512@gmail.com>
2025-03-21 17:11:43 +0300

    delete style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import "./App.css";
 import { AppRouter } from "./components/AppRouter";
 import { observer } from "mobx-react-lite";
 import { useContext, useEffect, useState } from "react";
