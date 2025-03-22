9ec1a556df8a400b8c93648743e39b52d56eb8d4
Save1i <savva0512@gmail.com>
2025-03-22 17:29:05 +0300

    some changes

--- server/index.js
+++ server/index.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const modules = require("./modules/modules")
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandingMinddleware')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const modules = require("./modules/modules");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandingMinddleware");
+const path = require("path");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, "static")))
-app.use(fileUpload({}))
-app.use('/api', router)
-
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
+app.use("/api", router);
 
 // обработчик ошибок, последний middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, ()=> console.log(`str ${PORT}`))
-    }catch(e) {
-        console.log(e);
-    }
-}
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`str ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
