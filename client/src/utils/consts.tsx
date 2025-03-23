54d8d9b235db66de07767af3d87ff6e9dcd429ee
Save1i <savva0512@gmail.com>
2025-03-23 17:39:59 +0300

    added PROFILE_ROUTE

--- client/src/utils/consts.tsx
+++ client/src/utils/consts.tsx
@@ -3,5 +3,6 @@ export const LOGIN_ROUTE = "/login";
 export const REGISTRATION_ROUTE = "/registration";
 export const HOME_ROUTE = "/";
 export const TEST_ROUTE = "/test";
+export const PROFILE_ROUTE = "/profile";
 
 export const SERVER_URL = "http://localhost:5000/";
