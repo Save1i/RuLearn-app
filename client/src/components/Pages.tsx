7afdca221bcb0bff223e0ad5599cb786e3a96a66
Save1i <savva0512@gmail.com>
2025-03-23 17:31:40 +0300

    added styles and some changes

--- client/src/components/Pages.tsx
+++ client/src/components/Pages.tsx
@@ -4,6 +4,9 @@ import { Context } from "../main";
 import { postTaskProgress } from "../http/postTaskProgress";
 import { useNavigate } from "react-router-dom";
 import { HOME_ROUTE } from "../utils/consts";
+import styles from "../styles/pages.module.css";
+import { IoIosCloseCircle } from "react-icons/io";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 interface AnswersProps {
   answer: string;
@@ -37,16 +40,30 @@ const Pages: React.FC<AnswersProps> = observer(({ answer, correctAnswer, userId,
   console.log(home.isPage);
 
   return (
-    <div>
-      <p>{answer}</p>
+    <div className={styles.pages}>
+      <div className={styles.pages__header}>
+        {answer === "Ошибка" ? (
+          <>
+            <IoIosCloseCircle className={`${styles.pages__svg} ${styles.incorrect}`} />
+            <p className={`${styles.pages__answer} ${styles.incorrect}`}>{answer}</p>
+          </>
+        ) : (
+          <>
+            <IoIosCheckmarkCircle className={`${styles.pages__svg} ${styles.correct}`} />
+            <p className={`${styles.pages__answer} ${styles.correct}`}>{answer}</p>
+          </>
+        )}
+      </div>
+
       {answer === "Ошибка" && (
         <div className="">
-          <p>Правильный ответ:</p>
-          <p>{correctAnswer}</p>
+          <p className={styles.pages__answer}>Правильный ответ:</p>
+          <p className={styles.pages_text}>{correctAnswer}</p>
         </div>
       )}
-
-      <button onClick={hasNextPage ? nextPage : completeTask}>{home.isPage + 1}</button>
+      <button className={styles.pages__btn} onClick={hasNextPage ? nextPage : completeTask}>
+        {hasNextPage ? "ПРОДОЛЖИТЬ" : "ЗАВЕРШИТЬ"}
+      </button>
     </div>
   );
 });
