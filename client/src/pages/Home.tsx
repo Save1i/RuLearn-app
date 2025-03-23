3d8d7b982b652af05b9860dc3492af4162d2f2e1
Save1i <savva0512@gmail.com>
2025-03-23 17:32:50 +0300

    added unLogin page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,11 +2,13 @@ import { useContext, useEffect } from "react";
 import Section from "./Section";
 import { Context } from "../main";
 import styles from "../styles/home.module.css";
-import LogInLogOut from "./LogInLogOut";
 import { fetchSections, fetchTaskProgress } from "../http/homeAPI";
 import { observer } from "mobx-react-lite";
 import { getUserId } from "../http/getUserId";
 import NavBarBottom from "../components/NavBarBottom";
+import Header from "../components/Header";
+import Statistics from "../components/Statistics";
+import LogInLogOut from "../components/LogInLogOut";
 
 const Home = observer(() => {
   const { home, user } = useContext(Context);
@@ -19,19 +21,40 @@ const Home = observer(() => {
 
   return (
     <div className={styles.home}>
-      <div className={styles.home__inner}>
-        <LogInLogOut />
-        <div className={styles.sections__container}>
-          {home.isSections.length > 0 ? (
-            home.isSections.map((section) => (
-              <Section key={section.id} section={section} tasksP={home.isTaskProgress} />
-            ))
-          ) : (
-            <p>Разделов пока нет</p>
-          )}
+      {user.isAuth ? (
+        <>
+          <Header />
+          <div className={styles.home__inner}>
+            <Statistics />
+
+            <div className={styles.sections}>
+              <h1 className={styles.sections__title}>Задания и практика</h1>
+              <div className={styles.sections__container}>
+                {home.isSections.length > 0 ? (
+                  home.isSections.map((section) => (
+                    <Section key={section.id} section={section} tasksP={home.isTaskProgress} />
+                  ))
+                ) : (
+                  <p>Разделов пока нет</p>
+                )}
+              </div>
+            </div>
+            <NavBarBottom />
+          </div>
+        </>
+      ) : (
+        <div
+          style={{
+            height: "100vh",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <LogInLogOut />
         </div>
-        <NavBarBottom />
-      </div>
+      )}
     </div>
   );
 });
