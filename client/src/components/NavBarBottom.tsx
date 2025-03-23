fec45f4e0f256b42ea4cfdff26fe363cda45185f
Save1i <savva0512@gmail.com>
2025-03-23 17:31:02 +0300

    added navigate

--- client/src/components/NavBarBottom.tsx
+++ client/src/components/NavBarBottom.tsx
@@ -3,22 +3,44 @@ import { FiHome } from "react-icons/fi";
 import { TbSwipe } from "react-icons/tb";
 import { LuNotebookPen } from "react-icons/lu";
 import { GoPerson } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+import { HOME_ROUTE, PROFILE_ROUTE } from "../utils/consts";
 
 const NavBarBottom = () => {
+  const navigate = useNavigate();
+  const path = window.location.pathname;
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navBar__container}>
         <li className={styles.navBar__el}>
-          <FiHome className={`${styles.navBar__el_svg} `} />
+          <FiHome
+            onClick={() => navigate(HOME_ROUTE)}
+            id="/"
+            style={path === "/" ? { color: "#ff6f04" } : { color: "#899bb5" }}
+            className={`${styles.navBar__el_svg} `}
+          />
         </li>
         <li className={styles.navBar__el}>
-          <TbSwipe className={`${styles.navBar__el_svg} ${styles.full} ${styles.bold_l}`} />
+          <TbSwipe
+            id="/swipe"
+            style={path === "/swipe" ? { color: "#ff6f04" } : { color: "#899bb5" }}
+            className={`${styles.navBar__el_svg} ${styles.full} ${styles.bold_l}`}
+          />
         </li>
         <li className={styles.navBar__el}>
-          <LuNotebookPen className={styles.navBar__el_svg} />
+          <LuNotebookPen
+            id="/words"
+            style={path === "/words" ? { color: "#ff6f04" } : { color: "#899bb5" }}
+            className={styles.navBar__el_svg}
+          />
         </li>
         <li className={styles.navBar__el}>
-          <GoPerson className={`${styles.navBar__el_svg} ${styles.bold} ${styles.full}`} />
+          <GoPerson
+            onClick={() => navigate(PROFILE_ROUTE)}
+            id="/profile"
+            style={path === "/profile" ? { color: "#ff6f04" } : { color: "#899bb5" }}
+            className={`${styles.navBar__el_svg} ${styles.bold} ${styles.full}`}
+          />
         </li>
       </ul>
     </nav>
