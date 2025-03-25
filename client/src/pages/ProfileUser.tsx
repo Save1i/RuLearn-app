de5ce4346876d1921f6d9f269fe10d0011b644cf
Save1i <savva0512@gmail.com>
2025-03-25 17:16:31 +0300

    added loginLogOut

--- client/src/pages/ProfileUser.tsx
+++ client/src/pages/ProfileUser.tsx
@@ -1,8 +1,10 @@
+import LogInLogOut from "../components/LogInLogOut";
 import NavBarBottom from "../components/NavBarBottom";
 
 const ProfileUser = () => {
   return (
     <div>
+      <LogInLogOut />
       <NavBarBottom />
     </div>
   );
