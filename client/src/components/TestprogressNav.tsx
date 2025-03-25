711ff8d2fd637da944009822697e49176d27759f
Save1i <savva0512@gmail.com>
2025-03-25 17:12:45 +0300

    create line progress

---
+++ client/src/components/TestprogressNav.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import styles from "../styles/testProgressNav.module.css";
+import { Context } from "../main";
+import { observer } from "mobx-react-lite";
+
+const TestprogressNav = observer(() => {
+  const { home } = useContext(Context);
+  const pageCount = Math.ceil(home.isTotalCount / home.isLimit);
+  const progress = (100 / pageCount) * (home.isPage - 1);
+
+  return (
+    <div className={styles.progress}>
+      <div className={styles.progress__line} style={{ width: `${progress}%` }}></div>
+    </div>
+  );
+});
+
+export default TestprogressNav;
