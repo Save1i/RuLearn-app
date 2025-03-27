7941c636da366387332c0a0558d3e55fc1629886
Save1i <savva0512@gmail.com>
2025-03-27 10:53:21 +0300

    added: showTest as prop to close test comp and added timeOut

--- client/src/components/CloseToHomeBtn.tsx
+++ client/src/components/CloseToHomeBtn.tsx
@@ -3,12 +3,22 @@ import styles from "../styles/closeToHomeBtn.module.css";
 import { IoCloseOutline } from "react-icons/io5";
 import { HOME_ROUTE } from "../utils/consts";
 
-const CloseToHomeBtn = () => {
+type ChildProps = {
+  showTest: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CloseToHomeBtn = ({showTest}: ChildProps) => {
+
   const navigate = useNavigate();
   return (
     <button className={styles.close__btn}>
       <IoCloseOutline
-        onClick={() => navigate(HOME_ROUTE)}
+        onClick={() => {
+          showTest(false)
+          setTimeout(() => {
+            navigate(HOME_ROUTE)
+          }, 200)
+        }}
         style={{ height: "34px", width: "34px" }}
       />
     </button>
