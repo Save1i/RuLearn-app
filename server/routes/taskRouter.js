ac3e2a7571387764805c077194afa05937cf6768
Save1i <savva0512@gmail.com>
2025-03-31 17:25:18 +0300

    update: auth

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -5,7 +5,7 @@ const checkRole = require("../middleware/chekRoleMiddleware");
 const authMiddleware = require("../middleware/AuthMiddleware");
 
 router.post("/", checkRole("ADMIN"), taskController.create);
-router.get("/", authMiddleware, taskController.getAll);
+router.get("/", taskController.getAll);
 router.get("/:id", authMiddleware, taskController.getOne);
 
 module.exports = router;
