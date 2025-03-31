dd33e3b386b5f7d1a0d3e0aaf63d9f36ca636a57
Save1i <savva0512@gmail.com>
2025-03-31 17:21:52 +0300

    update: port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandingMinddleware");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
