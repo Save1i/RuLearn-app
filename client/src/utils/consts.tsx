2a959074e7ca6b2fb5bd22100ee182bd564a519a
Save1i <savva0512@gmail.com>
2025-03-31 14:48:25 +0300

    update: for Neon

--- client/src/utils/consts.tsx
+++ client/src/utils/consts.tsx
@@ -6,4 +6,4 @@ export const TEST_ROUTE = "/test";
 export const PROFILE_ROUTE = "/profile";
 
 // export const SERVER_URL = "http://localhost:5000/";
-export const SERVER_URL = "postgresql://neondb_owner:npg_KjYR1rcSfwt2@ep-soft-credit-a85vqt43-pooler.eastus2.azure.neon.tech/neondb?sslmode=require";
+export const SERVER_URL = "http://rulearn.up.railway.app";
