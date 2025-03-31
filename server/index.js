b77e80beac2dcba5c24e5f2ad1dedc8dbf331750
Save1i <savva0512@gmail.com>
2025-03-31 16:21:13 +0300

    update: cors

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({message: "YOOOOOO!"})
