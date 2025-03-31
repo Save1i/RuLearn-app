81de4d7312781f25948513d186478f5772668197
Save1i <savva0512@gmail.com>
2025-03-31 15:20:35 +0300

    update: cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
