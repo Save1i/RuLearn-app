6bd9fd5ef19614761322ba7083c707cd570b7cb1
Save1i <savva0512@gmail.com>
2025-04-02 16:22:16 +0300

    v5

--- server/routes/testRouter.js
+++ server/routes/testRouter.js
@@ -1,7 +1,7 @@
 const Router = require("express");
 const router = new Router();
 const testController = require("../controllers/testController");
-const checkRole = require("../middleware/chekRoleMiddleware"); // Это можно оставить, если нужно
+// const checkRole = require("../middleware/chekRoleMiddleware"); // Это можно оставить, если нужно
 // const authMiddleware = require("../middleware/AuthMiddleware"); // Убираем
 
 router.post("/", testController.create); // можно добавить checkRole("Admin")
