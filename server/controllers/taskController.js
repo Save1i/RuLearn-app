7c09ea815e4141aef86bdfcf9a23ed9684bfe31b
Save1i <savva0512@gmail.com>
2025-04-03 21:49:34 +0300

    v4

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -3,20 +3,25 @@ const ApiError = require("../error/apiError");
 
 class TaskController {
   async create(req, res) {
-    try {
-    const { name, type, duration, sectionId } = req.body;
-
-    const task = await Task.create({ name, type, duration, sectionId});
-    return res.json(task);
+      try {
+        const { name, type, duration, sectionId } = req.body;
+    
+        // Попробуйте создать задачу
+        const task = await Task.create({ name, type, duration, sectionId });
+        
+        // Возвращаем созданную задачу
+        return res.json(task);  // Статус 201 - задача успешно создана
     } 
     catch (error) {
-        if (error.name === 'SequelizeUniqueConstraintError') {
-          // Обработать ошибку дублирования
-          console.log('Задача с таким именем уже существует');
-        } else {
-          // Обработать другие ошибки
-          console.log(error);
-        }
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        // Обрабатываем ошибку дублирования уникального значения
+        console.log('Задача с таким именем уже существует');
+        return res.status(400).json({ message: 'Задача с таким именем уже существует' });
+      } else {
+        // Обрабатываем все другие ошибки
+        console.error(error);
+        return res.status(500).json({ message: 'Произошла ошибка при создании задачи' });
+      }
     }
   }
 
