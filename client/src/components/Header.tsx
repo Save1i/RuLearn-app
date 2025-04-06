fbabf9905b6ab0c16afff4681067182124316845
Save1i <savva0512@gmail.com>
2025-04-06 12:50:06 +0300

    add: add scroll onClick header logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,23 @@
 import styles from "../styles/header.module.css";
 
 const Header = () => {
+
+  const scrollToTop = () => {
+    const scrollStep = -window.scrollY / 20;
+    const interval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(interval);
+      }
+    }, 10);
+  };
+  
+
   return (
     <div className={styles.header}>
       <div className={styles.header__inner}>
-        <h1 className={styles.header__logo}>RuLearn</h1>
+        <h1 className={styles.header__logo} onClick={scrollToTop}>RuLearn</h1>
         <button className={styles.header__btn}>ПРЕМИУМ</button>
       </div>
     </div>
