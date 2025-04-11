22ea18d39f222ed0c061002c827e154edd9161ca
Save1i <savva0512@gmail.com>
2025-04-11 15:18:01 +0300

    update: header to top

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,14 +3,18 @@ import styles from "../styles/header.module.css";
 const Header = () => {
 
   const scrollToTop = () => {
-    window.scrollBy(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    
   };
   
 
   return (
     <div className={styles.header}>
       <div className={styles.header__inner}>
-        <h1 className={styles.header__logo} onClick={scrollToTop}>RuLearn</h1>
+        <h1 className={styles.header__logo} onClick={()=>scrollToTop()}>RuLearn</h1>
         <button className={styles.header__btn}>ПРЕМИУМ</button>
       </div>
     </div>
