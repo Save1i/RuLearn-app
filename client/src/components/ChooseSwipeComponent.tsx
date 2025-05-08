3bb9e34808d9248d0846c09a569352c5f0a0ba51
Save1i <savva0512@gmail.com>
2025-05-08 10:55:34 +0300

    create

---
+++ client/src/components/ChooseSwipeComponent.tsx
@@ -0,0 +1,54 @@
+import { GoPencil } from "react-icons/go";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { RxCounterClockwiseClock } from "react-icons/rx";
+import styles from "../styles/ChooseSwipeComponent.module.css"
+import { useNavigate } from "react-router-dom";
+import { CHOOSECATEGORY_ROUTE } from "../utils/consts";
+
+interface isProps {
+  icon: string;
+  title: string;
+  info?: string;
+}
+
+const ChooseSwipeComponent = ({ icon, title, info }: isProps) => {
+    const navigate = useNavigate()
+
+    const renderIcon = () => {
+        switch (icon) {
+        case "GoPencil":
+            return <GoPencil className={styles.icon} style={{color:"#8B8B8B"}}/>;
+        case "IoAddCircleOutline":
+            return <IoAddCircleOutline className={styles.icon} style={{color:"#FF6F04"}}/>;
+        case "RxCounterClockwiseClock":
+            return <RxCounterClockwiseClock className={styles.icon} style={{color:"#0077B6"}}/>;
+        default:
+            return null;
+        }
+    };
+
+    const renderRouter = () => {
+        switch (icon) {
+        case "GoPencil":
+            return navigate(CHOOSECATEGORY_ROUTE)
+        case "IoAddCircleOutline":
+            return <IoAddCircleOutline className={styles.icon} style={{color:"#FF6F04"}}/>;
+        case "RxCounterClockwiseClock":
+            return <RxCounterClockwiseClock className={styles.icon} style={{color:"#0077B6"}}/>;
+        default:
+            return null;
+        }
+    };
+
+  return (
+    <div className={styles.button} onClick={() => renderRouter()}>
+      {renderIcon()}
+      <div className={styles.button__text}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.info}>{info}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseSwipeComponent;
