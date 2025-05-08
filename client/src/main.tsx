553bbe50ea19157d5c78abc92dd5eb71a2ec451c
Save1i <savva0512@gmail.com>
2025-05-08 10:55:02 +0300

    delete: context, and change import directore for Context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,12 @@
-import { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import UserStore from "./store/UserStore";
 import HomeStore from "./store/HomeStore";
+import { Context } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-interface ContextProps {
-  user: UserStore;
-  home: HomeStore;
-}
-
-export const Context = createContext<ContextProps>({
-  user: new UserStore(),
-  home: new HomeStore(),
-});
 
 root.render(
   <Context.Provider
