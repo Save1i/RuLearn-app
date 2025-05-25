59c4725c667dac3a4da5a1f04e18b75ff6f7be9a
Save1i <savva0512@gmail.com>
2025-05-25 13:28:04 +0300

    add: confetti, and home

--- client/src/pages/LearnNewWords.tsx
+++ client/src/pages/LearnNewWords.tsx
@@ -2,18 +2,52 @@ import SwipeCard from "../components/SwipeCard"
 import HeaderNav from '../components/HeaderNav'
 import TestprogressNav from '../components/TestprogressNav'
 import styles from '../styles/learnNewWord.module.css'
+import ConfettiExplosion from 'react-confetti-explosion';
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../context";
+import { observer } from "mobx-react-lite";
 
 
-const LearnNewWords = () => {
+
+const LearnNewWords = observer(() => {
+  const { home } = useContext(Context);
+
+  const [isExploding, setIsExploding] = useState(false);
+
+  useEffect(() => {
+    console.log(home.isTotalCount)
+    console.log(home.isPage)
+    if(home.isTotalCount > 0 && home.isTotalCount < home.isPage) {
+      setTimeout(() => {
+        setIsExploding(true)
+      }, 500)
+    }
+}, [home.isTotalCount, home.isPage])
+
   return (
     <div>
       <HeaderNav name='Учить новые слова'/>
         <div className={styles.learn__progress}>
-          <TestprogressNav />
+          {
+          isExploding ? <ConfettiExplosion 
+          force={0.1} 
+          particleCount={250}
+          width={1000}
+          particleSize={8}
+          duration={4000}
+          style={{
+  position: 'absolute',
+  top: '45px',
+  left: '50%',
+  zIndex: '1',
+  transform: 'translate(-50%, -50%)'
+}}
+          /> : <TestprogressNav totalCount={home.isTotalCount} isPage={home.isPage}/>
+          }
         </div>
       <SwipeCard/>
     </div>
   )
-}
+});
 
 export default LearnNewWords
